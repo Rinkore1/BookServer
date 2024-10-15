4944d7d817ad411eeedb02fa0dd05a235150f943
Rinkore28 <2213340209@qq.com>
2024-10-15 08:36:12 +0800

    init

---
+++ src/main/java/com/bookserver/deamon/DeamonApplication.java
@@ -0,0 +1,13 @@
+package com.bookserver.deamon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeamonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeamonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookserver/deamon/DeamonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookserver.deamon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeamonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
