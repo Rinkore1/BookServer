09b2cdc84a47672e8d9d09d53e5113bed346fc6d
KomeiDiSanXian <2211213431@qq.com>
2025-01-07 21:49:35 +0800

    fix: Remve unused code

--- src/main/java/com/bookserver/deamon/config/CorsConfig.java
+++ src/main/java/com/bookserver/deamon/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.bookserver.deamon.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
@@ -12,12 +11,6 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-            @Value("${cors.allowed-origins:*}") // 从配置文件加载允许的跨域来源
-            private String[] allowedOrigins;
-
-            @Value("${cors.allowed-methods:GET,POST,PUT,DELETE,OPTIONS}") // 从配置文件加载允许的跨域方法
-            private String[] allowedMethods;
-
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
