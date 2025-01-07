5046e407df0b511e4be53f3ecefb173bdf4979cd
KomeiDiSanXian <2211213431@qq.com>
2025-01-07 14:11:30 +0800

    feat: Add cors

---
+++ src/main/java/com/bookserver/deamon/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.bookserver.deamon.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Value("${cors.allowed-origins:*}") // 从配置文件加载允许的跨域来源
+            private String[] allowedOrigins;
+
+            @Value("${cors.allowed-methods:GET,POST,PUT,DELETE,OPTIONS}") // 从配置文件加载允许的跨域方法
+            private String[] allowedMethods;
+
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigins)
+                        .allowedMethods(allowedMethods)
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
